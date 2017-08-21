d175dcb710783f5969f0fafc0b4f51a2ac5753f7
maks-ushakov <ush.maksim@gmail.com>
2017-08-21 12:11:17 +0300

    Update styles

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
+/**
+ * Main page script.
+ */
+ 
 window.addEventListener('load', function() {
+
+	/** Get DOM element who represent CertStorage application */
 	var appRoot = document.getElementById('cert-storage');
+
+	/** Setup and run application */
 	CertStorage.init({
 		root: appRoot,
-		output: appRoot.querySelector('#cert__info'),
+		output: appRoot.querySelector('[data-role="certificate-info"]'),
 		templateInfo: document.querySelector('#info-template').innerHTML,
-		list: appRoot.querySelector('#cert__list-container'),
+		list: appRoot.querySelector('[data-role="certificate-list"]'),
 		templateList: document.querySelector('#list-template').innerHTML,
 	});
+
 	CertStorage.run();
 }, false)
